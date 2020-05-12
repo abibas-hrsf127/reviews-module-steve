28e7874c6a8750c4c1f9c1b7553f5aad0370229a
alexzhaohong <alexhong96@gmail.com>
2020-05-12 11:41:10 -0700

    Change Reviews id to ReviewsRoot id

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 // import './index.css'; no longer using global css
 import App from './components/App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('Reviews'));
+ReactDOM.render(<App/>, document.getElementById('ReviewsRoot'));
 
