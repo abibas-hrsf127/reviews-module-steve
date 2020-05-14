da76062e59bb46417bcb062cd363d0b6069feb31
alexzhaohong <alexhong96@gmail.com>
2020-05-13 17:17:36 -0700

    Add scripts

---
+++ dockerscripts.bash
@@ -0,0 +1,31 @@
+docker build -t reviewsfecserver .
+docker run -d -p 3003:3003 --name reviewscontainer reviewsfecserver
+docker run -d -p 2000:8080 -v $(pwd):/src/app --name psychic_reviewscontainer reviewsfecserver
+
+# Workflow
+docker build
+docker images 
+docker run
+open http://localhost:3003
+
+# 4-Containerized_Development_With_Volumes
+docker logs <container-name>
+docker volume ls
+docker volume prune
+# 3-Bundle_Your_App_Into_An_Image
+docker pull <image-name> : <version>
+docker history <image-name>
+docker build --tag <image-name>
+docker run -d -p <hostport:containerport> -v <$(pwd):directorypath> --rm --name <container-name> <image-name>
+docker restart <container-name>
+# 2-Long_Lived_Containers
+docker stop <container-name>
+docker start <container-name>
+docker rm -f <container-name>
+docker rmi <image-name>
+# 1-Introduction
+docker info
+docker images -a
+docker run <image-name>
+docker ps
+docker ps -a
