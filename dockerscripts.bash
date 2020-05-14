ad3b80aabdb079c001c05dfe0c43d28a4c2ab467
alexzhaohong <alexhong96@gmail.com>
2020-05-14 02:47:31 -0700

    Attempt Docker Compose

--- dockerscripts.bash
+++ dockerscripts.bash
@@ -2,6 +2,17 @@ docker build -t alexzhaohong/reviewsfecserver .
 docker run -d -p 3003:3003 --name reviewscontainer alexzhaohong/reviewsfecserver
 docker run -d -p 2000:8080 -v $(pwd):/src/app --name psychic_reviewscontainer alexzhaohong/reviewsfecserver
 
+# Compose Workflow
+docker-compose up -d --build
+open http://localhost:3003
+docker ps -a
+docker volume ls
+containername="reviews-module_reviews-module_1"
+echo $containername
+docker logs $containername
+docker exec -it $containername sh
+docker-compose down -v --rmi all
+
 # Workflow
 docker build
 docker images 
@@ -18,7 +29,14 @@ docker push alexzhaohong/reviewsfecserver:tagname
 docker pull alexzhaohong/reviewsfecserver:latest
 docker run -d -p 3003:3003 --name reviewscontainer alexzhaohong/reviewsfecserver
 open http://localhost:3003
-
+docker logs reviewscontainer
+docker exec -it reviewscontainer sh
+docker rm -f reviewscontainer
+docker rmi alexzhaohong/reviewsfecserver:latest
+# 6
+docker-compose up
+docker-compose up -d --build
+docker-compose down -v --rmi all
 # 5-Make_Multiple_Containers_Work_Together
 docker network ls
 docker network inspect <network-name> #IPv4 Addressing
