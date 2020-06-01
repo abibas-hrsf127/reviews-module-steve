921435aca4ee42a3dac75718527b6eff3d6e0476
Steve Marquez <steveantonio00@gmail.com>
2020-06-01 07:57:18 -0700

    Implement k6 GET stress test

--- k6_tests/stress_get.js
+++ k6_tests/stress_get.js
@@ -17,7 +17,6 @@ export let options = {
     { duration: "2m", target: 300 }, // around the breaking point
     { duration: "1m", target: 0 },   // scale down. Recovery stage
   ],
-
   thresholds: {
     http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
   },
