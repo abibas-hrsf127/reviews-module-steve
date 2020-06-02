d8b6bb9790a0efdee02914e55cc80a3fba4f4ea1
Steve Marquez <steveantonio00@gmail.com>
2020-06-01 23:43:26 -0700

    Install redis

--- k6_tests/stress_get.js
+++ k6_tests/stress_get.js
@@ -4,26 +4,32 @@ import { check, sleep } from "k6";
 
 var myErrorCounter = new Counter("my_error_counter");
 
-const randomIntBetween = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
 
 export let options = {
   stages: [
-    { duration: "2m", target: 100 }, // below normal load
-    { duration: "2m", target: 100 },
-    { duration: "2m", target: 200 }, // normal load
-    { duration: "2m", target: 200 },
-    { duration: "2m", target: 300 }, // around the breaking point
+    { duration: "1m", target: 100 }, // below normal load
+    { duration: "1m", target: 100 },
+    { duration: "1m", target: 200 }, // normal load
+    { duration: "1m", target: 200 }, // around the breaking point
+    { duration: "1m", target: 250 }, // around the breaking point
+    { duration: "1m", target: 250 }, // around the breaking point
     { duration: "1m", target: 0 },   // scale down. Recovery stage
   ],
-  thresholds: {
-    http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
-  },
-};
 
-export default function () {
-  const url = "http://localhost:3003/api/models";
+    // vus: 300,
+    // duration: '2m'
+
+  // thresholds: {
+    //   http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
+    // },
+  };
+  
+  export default function () {
+    const url = "http://localhost:3003/api/models";
+    const randomIntBetween = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    
   let res = http.get(
     `${url}/${Math.round(randomIntBetween(1, 10000000))}/reviews`
   );
@@ -31,4 +37,5 @@ export default function () {
   if (res.status === 404) {
     myErrorCounter.add(1);
   }
+  sleep(Math.random() * 30);
 }
